b7b19513882d172c3c20bd4e822a4f529b4320fd
Mohamad J. Makki <malanbaki@transforat.com>
2022-12-24 13:58:53 +0300

    Spring Core

---
+++ src/main/java/app/ecommerce/ecommercecore/ECommerceCoreApplication.java
@@ -0,0 +1,13 @@
+package app.ecommerce.ecommercecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/ecommerce/ecommercecore/ECommerceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ecommerce.ecommercecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
