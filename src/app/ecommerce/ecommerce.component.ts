8b701c67ee11ab57a2e830b24d774ada058f9699
Mohamad J. Makki <malanbaki@transforat.com>
2022-12-24 15:15:08 +0300

    finishing up the front

--- src/app/ecommerce/ecommerce.component.ts
+++ src/app/ecommerce/ecommerce.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ProductsComponent} from "./products/products.component";
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
+import {OrdersComponent} from "./orders/orders.component";
 
 @Component({
   selector: 'app-ecommerce',
   templateUrl: './ecommerce.component.html',
   styleUrls: ['./ecommerce.component.css']
 })
-export class EcommerceComponent {
+export class EcommerceComponent implements OnInit {
+  private collapsed = true;
+  orderFinished = false;
 
+  @ViewChild('productsC')
+  productsC: ProductsComponent | undefined;
+
+  @ViewChild('shoppingCartC')
+  shoppingCartC: ShoppingCartComponent | undefined;
+
+  @ViewChild('ordersC')
+  ordersC: OrdersComponent | undefined;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+  finishOrder(orderFinished: boolean) {
+    this.orderFinished = orderFinished;
+  }
+
+  reset() {
+    this.orderFinished = false;
+    // @ts-ignore
+    this.productsC.reset();
+    // @ts-ignore
+    this.shoppingCartC.reset();
+    // @ts-ignore
+    this.ordersC.paid = false;
+  }
 }
---
+++ src/app/ecommerce/services/EcommerceService.ts
@@ -0,0 +1,67 @@
+import {ProductOrder} from "../models/product-order.model";
+import {Subject} from "rxjs/internal/Subject";
+import {ProductOrders} from "../models/product-orders.model";
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class EcommerceService {
+  private productsUrl = "/api/products";
+  private ordersUrl = "/api/orders";
+
+  private productOrder: ProductOrder | undefined;
+  private orders: ProductOrders = new ProductOrders();
+
+  private productOrderSubject = new Subject();
+  private ordersSubject = new Subject();
+  private totalSubject = new Subject();
+
+  private total: number | undefined;
+
+  ProductOrderChanged = this.productOrderSubject.asObservable();
+  OrdersChanged = this.ordersSubject.asObservable();
+  TotalChanged = this.totalSubject.asObservable();
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllProducts() {
+    return this.http.get(this.productsUrl);
+  }
+
+  saveOrder(order: ProductOrders) {
+    return this.http.post(this.ordersUrl, order);
+  }
+
+  set SelectedProductOrder(value: ProductOrder) {
+    this.productOrder = value;
+    // @ts-ignore
+    this.productOrderSubject.next();
+  }
+
+  get SelectedProductOrder() {
+    // @ts-ignore
+    return this.productOrder;
+  }
+
+  set ProductOrders(value: ProductOrders | undefined) {
+    this.orders = value;
+    // @ts-ignore
+    this.ordersSubject.next();
+  }
+
+  get ProductOrders(): ProductOrders | undefined {
+    return this.orders;
+  }
+
+  get Total() {
+    // @ts-ignore
+    return this.total;
+  }
+
+  set Total(value: number) {
+    this.total = value;
+    // @ts-ignore
+    this.totalSubject.next();
+  }
+}
