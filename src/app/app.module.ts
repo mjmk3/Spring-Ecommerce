dd070cd152e653e3e6b1b9b6359f93661677737d
Mohamad J. Makki <malanbaki@transforat.com>
2022-12-24 15:17:57 +0300

    finishing up the front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { EcommerceComponent } from './ecommerce/ecommerce.component';
-import { ProductsComponent } from './ecommerce/products/products.component';
-import { OrdersComponent } from './ecommerce/orders/orders.component';
-import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.component';
+import {AppComponent} from './app.component';
+import {EcommerceComponent} from './ecommerce/ecommerce.component';
+import {ProductsComponent} from './ecommerce/products/products.component';
+import {ShoppingCartComponent} from './ecommerce/shopping-cart/shopping-cart.component';
+import {OrdersComponent} from './ecommerce/orders/orders.component';
+import {EcommerceService} from "./ecommerce/services/EcommerceService";
 
 @NgModule({
   declarations: [
     AppComponent,
     EcommerceComponent,
     ProductsComponent,
-    OrdersComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    OrdersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [EcommerceService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
